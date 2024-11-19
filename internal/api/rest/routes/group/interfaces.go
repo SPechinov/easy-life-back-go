bb0a32024477a6e8c3d292c34aa2be9a6dc98959
Sergei Pechinov <spechinov@gmail.com>
2024-11-19 17:38:17 +0100

    Save

--- internal/api/rest/routes/group/interfaces.go
+++ internal/api/rest/routes/group/interfaces.go
@@ -7,11 +7,11 @@ import (
 
 type useCases interface {
 	Add(ctx context.Context, entity entities.GroupAdd) (*entities.Group, error)
-	GetList(ctx context.Context, entity entities.GroupsGetList) ([]entities.Group, error)
-	GetFull(ctx context.Context, userID string, entity entities.GroupGet) (*entities.GroupFull, error)
-	Get(ctx context.Context, userID string, entity entities.GroupGetInfo) (*entities.Group, error)
-	GetUsersList(ctx context.Context, userID string, entity entities.GroupGetUsersList) ([]entities.GroupUser, error)
 	Patch(ctx context.Context, adminID string, entity entities.GroupPatch) error
+	Get(ctx context.Context, userID string, entity entities.GroupGetInfo) (*entities.Group, error)
+	GetFull(ctx context.Context, userID string, entity entities.GroupGet) (*entities.GroupFull, error)
+	GetList(ctx context.Context, entity entities.GroupsGetList) ([]entities.Group, error)
+	GetUsersList(ctx context.Context, userID string, entity entities.GroupGetUsersList) ([]entities.GroupUser, error)
 	InviteUser(ctx context.Context, adminID string, entity entities.GroupInviteUser) error
 	ExcludeUser(ctx context.Context, adminID string, entity entities.GroupExcludeUser) error
 }
--- internal/usecases/group/interfaces.go
+++ internal/usecases/group/interfaces.go
@@ -8,9 +8,9 @@ import (
 type groupService interface {
 	Add(ctx context.Context, entity entities.GroupAdd) (*entities.Group, error)
 	Patch(ctx context.Context, entity entities.GroupPatch) error
+	Get(ctx context.Context, entity entities.GroupGetInfo) (*entities.Group, error)
 	GetFull(ctx context.Context, entity entities.GroupGet) (*entities.GroupFull, error)
 	GetList(ctx context.Context, entity entities.GroupsGetList) ([]entities.Group, error)
-	Get(ctx context.Context, entity entities.GroupGetInfo) (*entities.Group, error)
 	GetUsersList(ctx context.Context, entity entities.GroupGetUsersList) ([]entities.GroupUser, error)
 	IsGroupAdmin(ctx context.Context, userID, groupID string) (bool, error)
 	GetGroupUser(ctx context.Context, userID, groupID string) (*entities.GroupUser, error)
