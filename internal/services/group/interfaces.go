7209a76253065c2e6465988d72d8ad97a0e590fb
Sergei Pechinov <spechinov@gmail.com>
2024-11-19 17:39:40 +0100

    Save

--- internal/services/group/interfaces.go
+++ internal/services/group/interfaces.go
@@ -5,19 +5,19 @@ import (
 	"go-clean/internal/entities"
 )
 
-type userService interface {
-	GetUser(ctx context.Context, data entities.UserGet) (*entities.User, error)
-}
-
 type groupDatabase interface {
 	Add(ctx context.Context, entity entities.GroupAdd) (groupID string, err error)
-	Get(ctx context.Context, entity entities.GroupGet) (*entities.Group, error)
 	Patch(ctx context.Context, entity entities.GroupPatch) error
-	IsGroupAdmin(ctx context.Context, userID, groupID string) (bool, error)
+	Get(ctx context.Context, entity entities.GroupGet) (*entities.Group, error)
 	GetList(ctx context.Context, entity entities.GroupsGetList) ([]entities.Group, error)
 	GetUsersList(ctx context.Context, entity entities.GroupGetUsersList) ([]entities.GroupUser, error)
 	GetGroupUser(ctx context.Context, userID, groupID string) (*entities.GroupUser, error)
 	InviteUser(ctx context.Context, entity entities.GroupInviteUser) error
 	ExcludeUser(ctx context.Context, entity entities.GroupExcludeUser) error
 	IsDeletedGroup(ctx context.Context, groupID string) bool
+	IsGroupAdmin(ctx context.Context, userID, groupID string) (bool, error)
+}
+
+type userService interface {
+	GetUser(ctx context.Context, data entities.UserGet) (*entities.User, error)
 }
