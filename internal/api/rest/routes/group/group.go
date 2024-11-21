96dca39d9a375b317b81faf135be65acbe6df548
Sergei Pechinov <spechinov@gmail.com>
2024-11-21 16:04:33 +0100

    Update group

--- internal/api/rest/routes/group/group.go
+++ internal/api/rest/routes/group/group.go
@@ -8,6 +8,19 @@ import (
 	"go-clean/internal/api/rest/utils"
 )
 
+const (
+	urlGroupAdd           = ""
+	urlGroupPatch         = "/:groupID"
+	urlGroupDelete        = "/:groupID/delete"
+	urlGroupDeleteConfirm = "/:groupID/delete-confirm"
+	urlGroupsList         = ""
+	urlGroup              = "/:groupID"
+	urlGroupInfo          = "/:groupID/info"
+	urlGroupUsers         = "/:groupID/users"
+	urlGroupInviteUser    = "/:groupID/invite-user"
+	urlGroupExcludeUser   = "/:groupID/exclude-user"
+)
+
 type restGroupController struct {
 	cfg      *config.Config
 	useCases useCases
@@ -24,16 +37,16 @@ func (controller *restGroupController) Register(router *echo.Group) {
 	authRouter := router.Group("/groups")
 	authRouter.Use(middlewares.AuthMiddleware(controller.cfg))
 
-	authRouter.POST("", utils.HandleWithValidate[AddDTO](validateAddDTO, controller.handlerAddGroup))
-	authRouter.PATCH("/:groupID", utils.HandleWithValidate[PatchDTO](validatePatchDTO, controller.handlerPatchGroup))
-	authRouter.DELETE("/:groupID/delete", utils.Handle(controller.handlerDelete))
-	authRouter.DELETE("/:groupID/delete-confirm", utils.HandleWithValidate[DeleteDTO](validateDeleteDTO, controller.handlerDeleteConfirm))
+	authRouter.POST(urlGroupAdd, utils.HandleWithValidate[AddDTO](validateAddDTO, controller.handlerAddGroup))
+	authRouter.PATCH(urlGroupPatch, utils.HandleWithValidate[PatchDTO](validatePatchDTO, controller.handlerPatchGroup))
+	authRouter.DELETE(urlGroupDelete, utils.Handle(controller.handlerDelete))
+	authRouter.DELETE(urlGroupDeleteConfirm, utils.HandleWithValidate[DeleteDTO](validateDeleteDTO, controller.handlerDeleteConfirm))
 
-	authRouter.GET("", utils.Handle(controller.handlerGetGroupsList))
-	authRouter.GET("/:groupID", utils.Handle(controller.handlerGetFullGroup))
-	authRouter.GET("/:groupID/info", utils.Handle(controller.handlerGetGroupInfo))
-	authRouter.GET("/:groupID/users", utils.Handle(controller.handlerGetGroupUsers))
+	authRouter.GET(urlGroupsList, utils.Handle(controller.handlerGetGroupsList))
+	authRouter.GET(urlGroup, utils.Handle(controller.handlerGetFullGroup))
+	authRouter.GET(urlGroupInfo, utils.Handle(controller.handlerGetGroupInfo))
+	authRouter.GET(urlGroupUsers, utils.Handle(controller.handlerGetGroupUsers))
 
-	authRouter.POST("/:groupID/invite-user", utils.HandleWithValidate[InviteUserDTO](validateInviteUserDTO, controller.handlerInviteUserInGroup))
-	authRouter.POST("/:groupID/exclude-user", utils.HandleWithValidate[ExcludeUserDTO](validateExcludeUserDTO, controller.handlerExcludeUserFromGroup))
+	authRouter.POST(urlGroupInviteUser, utils.HandleWithValidate[InviteUserDTO](validateInviteUserDTO, controller.handlerInviteUserInGroup))
+	authRouter.POST(urlGroupExcludeUser, utils.HandleWithValidate[ExcludeUserDTO](validateExcludeUserDTO, controller.handlerExcludeUserFromGroup))
 }
